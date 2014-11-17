7e4fa0af080ef63f113486f38f3ae84e6740a10c
Aymen MOUSSI <moussi.gl@gmail.com>
2014-11-17 16:08:58 +0100

    First Configuration

---
+++ src/main/java/com/moussi/controller/EventController.java
@@ -0,0 +1,16 @@
+package com.moussi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventController {
+
+	@RequestMapping(value="/greeting")
+	public String sayHello(Model model)
+	{
+		model.addAttribute("greeting", "Hello Moussi");
+		return "hello.jsp";
+	}
+}
